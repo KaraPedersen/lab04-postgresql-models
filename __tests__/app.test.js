faa38629e18dcd70226667d971231fce9ba413d9
KARA PEDERSEN <KJPED74@GMAIL.COM>
2021-06-10 17:58:07 -0700

    post route and test done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,28 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+// import Dog from '../lib/models/Dog.js';
 
-describe('demo routes', () => {
+// CRUD
+// C - create POST      INSERT
+// R - read   GET       SELECT
+// U - update PUT       UPDATE
+// D - delete DELETE    DELETE
+describe('dog routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates a dog via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/dogs')
+      .send({ name: 'spot', age: 5, weight: '20 lbs' });
+
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'spot',
+      age: 5,
+      weight: '20 lbs',
+    });
   });
 });
